f629e09db2e25badecde51674668d33639dbb960
Roman <rbalik341@gmail.com>
2019-01-22 18:26:33 +0200

    Instagram

--- app/router.js
+++ app/router.js
@@ -7,14 +7,15 @@ const Router = EmberRouter.extend({
 });
 
 Router.map(function () {
-    this.route('say-hi', {path: '/'});
-    this.route('skills');
-    this.route('calculator');
-    this.route('about');
-    this.route('posts', function () {
-        this.route('show', {path: '/:post_id'});
-    });
-    this.route('currencies');
-    this.route('post-body');
+  this.route('say-hi', {path: '/'});
+  this.route('skills');
+  this.route('calculator');
+  this.route('about');
+  this.route('posts', function () {
+      this.route('show', {path: '/:post_id'});
+  });
+  this.route('currencies');
+  this.route('post-body');
+  this.route('photos');
 });
 export default Router;
