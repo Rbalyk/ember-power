40caee90836e7b230ad5b42e61972c71d14e7e64
Roman <rbalik341@gmail.com>
2019-01-24 18:22:44 +0200

    fixed shop-list input validation

--- app/components/shop-list.js
+++ app/components/shop-list.js
@@ -8,12 +8,13 @@ export default Component.extend({
     actions:{
         addFruit(val){
             let arrayFruit = this.get('arrayOfFruit');
-            if (val !== undefined && !arrayFruit.includes(val)) {
-                arrayFruit.pushObject(val)
+            if ((val !== undefined && val !== "") && !arrayFruit.includes(val)  ) {
+                arrayFruit.pushObject(val);
             }
 
-            if(val == undefined){
+            if(val == undefined || val == ""){
                 alert(`You didn't write any item`);
+                console.log(val)
             }
         }
     }
