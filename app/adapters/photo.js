9f80422236d7c5968c5eba1be6b5dd8b45a81b81
roman <rbalik341@gmail.com>
2019-06-02 22:23:36 +0300

    New instagram token

--- app/adapters/photo.js
+++ app/adapters/photo.js
@@ -4,6 +4,6 @@ export default DS.RESTAdapter.extend({
     host:'https://api.instagram.com',
     namespace: 'v1/users/self/media/recent',
     pathForType(){
-        return '?access_token=1554840227.1677ed0.0eeb346a248d45c3a4b80c30ca2b2f5e';
+        return '?access_token=1554840227.1677ed0.27d0b691cd4c449a8b0680601b385335';
     }
 });
